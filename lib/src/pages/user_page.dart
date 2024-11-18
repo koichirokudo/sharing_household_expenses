d33d04e75a05fdcab2f14e0e635e1ba5feed5ce1
Koichiro Kudo <koichirokudo746@gmail.com>
2024-11-18 14:42:13 +0900

    ユーザー情報画面のレイアウト更新

--- lib/src/pages/user_page.dart
+++ lib/src/pages/user_page.dart
@@ -21,100 +21,76 @@ class UserPageState extends State<UserPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('ユーザー情報'),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Container(
-            height: 420,
-            width: 400,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12.0),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 10,
-                  offset: Offset(0, 4),
+        child: Container(
+          height: 420,
+          width: 400,
+          decoration: const BoxDecoration(
+            color: Color(0x00FFE7D4),
+          ),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const SizedBox(height: 16.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'ユーザー名',
+                prefixIcon: Icon(Icons.account_circle),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'メールアドレス',
+                prefixIcon: Icon(Icons.mail),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              obscureText: _isObscure,
+              decoration: InputDecoration(
+                  labelText: 'パスワード',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: _toggleObscure,
+                  )),
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              obscureText: _isObscure,
+              decoration: InputDecoration(
+                  labelText: '再度パスワード',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                        _isObscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: _toggleObscure,
+                  )),
+            ),
+            const SizedBox(height: 32.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  child: const Text('保存'),
+                  onPressed: () {
+                    // TODO: 保存処理
+                    Navigator.pop(context, 'テスト');
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('戻る'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
               ],
             ),
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                          'ユーザー情報',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          )
-                      ),
-                      Icon(
-                        Icons.account_circle,
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 16.0),
-                  const Divider(color: Colors.grey),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'ユーザー名',
-                      prefixIcon: Icon(Icons.account_circle),
-                    ),
-                  ),
-                  const SizedBox(height: 32.0),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'メールアドレス',
-                      prefixIcon: Icon(Icons.mail),
-                    ),
-                  ),
-                  const SizedBox(height: 32.0),
-                  TextFormField(
-                    obscureText: _isObscure,
-                    decoration: InputDecoration(
-                        labelText: 'パスワード',
-                        prefixIcon: const Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                              _isObscure ? Icons.visibility : Icons.visibility_off
-                          ),
-                          onPressed: _toggleObscure,
-                        )
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      MaterialButton(
-                        child: const Text('保存'),
-                        onPressed: () {
-                          // TODO: 保存処理
-                          Navigator.pop(context, 'テスト');
-                        },
-                      ),
-                      MaterialButton(
-                        child: const Text('戻る'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  ),
-                ]
-            ),
-          ),
+          ]),
         ),
       ),
     );

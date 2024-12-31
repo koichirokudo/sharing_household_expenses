ba488be0c21a67c5556184594fcc53d598f9678e
Koichiro Kudo <koichirokudo746@gmail.com>
2024-12-31 18:25:50 +0900

    ホーム画面に収支情報を表示する

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:sharing_household_expenses/services/transaction_service.dart';
 import 'package:sharing_household_expenses/src/pages/first_group_invite_page.dart';
 import 'package:sharing_household_expenses/src/pages/user_page.dart';
@@ -16,13 +16,40 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  bool _isLoading = false;
   final Session? session = supabase.auth.currentSession;
   late TransactionService transactionService;
+  late List<Map<String, dynamic>> transactions = [];
+  late List<Map<String, dynamic>> profiles = [];
+  late Map<String, dynamic> profile = {};
+  final userId = supabase.auth.currentUser!.id;
+  double sharedIncome = 0;
+  double sharedExpense = 0;
+  double privateIncome = 0;
+  double privateExpense = 0;
+  String sharedIncomeAmount = '';
+  String sharedExpenseAmount = '';
+  String privateIncomeAmount = '';
+  String privateExpenseAmount = '';
+
+  Future<void> _getProfiles() async {
+    try {
+      await Future.delayed(Duration(milliseconds: 700));
+      profiles = await supabase
+          .from('profiles')
+          .select()
+          .eq('group_id', profile['group_id']);
+    } catch (error) {
+      if (mounted) {
+        context.showSnackBarError(message: '$error');
+      }
+    }
+  }
 
   Future<void> _checkUserGroupStatus() async {
     final userId = supabase.auth.currentUser!.id;
     try {
-      final profile =
+      profile =
           await supabase.from('profiles').select('*').eq('id', userId).single();
 
       if (profile['group_id'] == null) {
@@ -40,11 +67,59 @@ class HomePageState extends State<HomePage> {
     }
   }
 
+  Future<void> _fetchTransactions() async {
+    try {
+      final currentMonth = DateFormat('yyyy/MM').format(DateTime.now());
+      final List<Map<String, dynamic>>? data =
+          await transactionService.fetchMonthlyData(
+              profile['group_id'], convertMonthToDateTime(currentMonth));
+      setState(() {
+        transactions = data ?? [];
+      });
+    } on PostgrestException catch (error) {
+      if (mounted) {
+        context.showSnackBarError(message: '$error');
+      }
+    }
+  }
+
+  void _calcTotal() {
+    for (var item in transactions) {
+      if (item['share'] == true) {
+        if (item['type'] == 'income') {
+          sharedIncome += item['amount'];
+        } else if (item['type'] == 'expense') {
+          sharedExpense += item['amount'];
+        }
+      } else {
+        if (item['type'] == 'income') {
+          privateIncome += item['amount'];
+        } else if (item['type'] == 'expense') {
+          privateExpense += item['amount'];
+        }
+      }
+    }
+    sharedIncomeAmount =
+        context.convertToYenFormat(amount: sharedIncome.round());
+    sharedExpenseAmount =
+        context.convertToYenFormat(amount: sharedExpense.round());
+    privateIncomeAmount =
+        context.convertToYenFormat(amount: privateIncome.round());
+    privateExpenseAmount =
+        context.convertToYenFormat(amount: privateExpense.round());
+  }
+
   @override
   void initState() {
     super.initState;
     transactionService = TransactionService(supabase);
-    _checkUserGroupStatus();
+    _initializeData();
+  }
+
+  Future<void> _initializeData() async {
+    await _checkUserGroupStatus();
+    await _fetchTransactions();
+    _calcTotal();
   }
 
   // TODO: サーバーからデータを取得する
@@ -73,167 +148,19 @@ class HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        const Text(
-          '今月の収支',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          const SizedBox(height: 16),
+          const Text(
+            '今月の収支',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                width: 150,
-                height: 150,
-                child: PieChart(
-                  PieChartData(
-                    sections: [
-                      PieChartSectionData(
-                        color: Colors.green,
-                        value: 0.3,
-                        title: '収入',
-                        radius: 50,
-                      ),
-                      PieChartSectionData(
-                        color: Colors.red,
-                        value: 0.7,
-                        title: '支出',
-                        radius: 50,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 150,
-                height: 150,
-                child: PieChart(
-                  PieChartData(
-                    sections: [
-                      PieChartSectionData(
-                        color: Colors.green,
-                        value: 0.3,
-                        title: 'グループの収入',
-                        radius: 50,
-                      ),
-                      PieChartSectionData(
-                        color: Colors.red,
-                        value: 0.7,
-                        title: 'グループの支出',
-                        radius: 50,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        // 個人の収支
-        Card(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                  const Icon(
-                    Icons.account_circle,
-                    color: Colors.blue,
-                    size: 32,
-                  ),
-                  const SizedBox(width: 16),
-                  const Text(
-                    '個人',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ]),
-              ),
-              const Divider(height: 1, color: Colors.black12),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.trending_up,
-                          color: Colors.green,
-                          size: 32,
-                        ),
-                        const SizedBox(width: 16),
-                        const Text(
-                          '収入',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Text(
-                      '¥100,000',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.trending_down,
-                          color: Colors.red,
-                          size: 32,
-                        ),
-                        const SizedBox(width: 16),
-                        const Text(
-                          '支出',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Text(
-                      '¥100,000',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 8),
-        // グループの収支
-        Card(
-          child: Column(
+          // 個人の収支
+          Column(
             children: [
               Padding(
                 padding: const EdgeInsets.all(16.0),
@@ -279,8 +206,8 @@ class HomePageState extends State<HomePage> {
                         ),
                       ],
                     ),
-                    const Text(
-                      '¥100,000',
+                    Text(
+                      sharedIncomeAmount,
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
@@ -313,8 +240,8 @@ class HomePageState extends State<HomePage> {
                         ),
                       ],
                     ),
-                    const Text(
-                      '¥100,000',
+                    Text(
+                      sharedExpenseAmount,
                       style: TextStyle(
                         fontSize: 24,
                         fontWeight: FontWeight.bold,
@@ -326,8 +253,103 @@ class HomePageState extends State<HomePage> {
               ),
             ],
           ),
-        ),
-      ]),
+          const SizedBox(height: 8),
+          // グループの収支
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  const Icon(
+                    Icons.account_circle,
+                    color: Colors.blue,
+                    size: 32,
+                  ),
+                  const SizedBox(width: 16),
+                  const Text(
+                    '個人',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ]),
+              ),
+              const Divider(height: 1, color: Colors.black12),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.trending_up,
+                          color: Colors.green,
+                          size: 32,
+                        ),
+                        const SizedBox(width: 16),
+                        const Text(
+                          '収入',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      privateIncomeAmount,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.trending_down,
+                          color: Colors.red,
+                          size: 32,
+                        ),
+                        const SizedBox(width: 16),
+                        const Text(
+                          '支出',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      privateExpenseAmount,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
